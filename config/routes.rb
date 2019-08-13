c20555cfbb4f1276174247f54bd221d699da9ad9
syiffa <syiffaadin87@gmail.com>
2019-08-13 12:44:26 +0700

    index user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get '/index', to: 'home#index'
-  get '/category', to: 'navbar#category'
-  get '/article', to: 'news#article'
-  post '/komentar', to: 'comment#create'
-
-  end
+  get '/', to: 'home#index'
+  get '/article/:id', to: 'article#show'
+  post '/comment', to: 'comment#create'
+  get '/category/:id', to: 'category#show'
+end
